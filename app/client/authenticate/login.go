586adfa808a895b42e683af5c46af61bb7fae870
PaulMatencio <paulo.matencio@gmail.com>
2016-09-25 13:05:22 +0200

    add client basic authentication and secure fetch

--- app/client/authenticate/login.go
+++ app/client/authenticate/login.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"crypto/tls"
+	json "encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+
+	gopass "github.com/howeyc/gopass"
+	//terminal "golang.org/x/crypto/ssh/terminal"
 )
 
 var (
 	myurl, userid, password string
-	token                   []byte
+	token, pass             []byte
+	err                     error
+	resetc                  string
 )
 
+type MyToken struct {
+	Token string
+}
+
 func usage() {
-	usage := "\nUsage: login  -wwww  string -u  userid -p password" +
+	usage := "\nUsage: authenticate  -www  <url> -u  <userid> " +
 		"\n\nDefault Options:\n"
 	fmt.Println(usage)
 	flag.PrintDefaults()
@@ -24,50 +34,69 @@ func usage() {
 }
 
 func main() {
-
 	flag.Usage = usage
-	flag.StringVar(&myurl, "www", "", "web page to login")
-	flag.StringVar(&userid, "c", "paul", "user idrntification ")
-	flag.StringVar(&password, "p", "oct@@@1998", "user password")
-
+	flag.StringVar(&myurl, "www", "", "Web server url")
+	flag.StringVar(&userid, "u", "", "User identification ")
+	flag.StringVar(&resetc, "r", "", "Reset jwt token")
 	flag.Parse()
-	if len(myurl) == 0 {
+
+	if len(resetc) > 0 {
+		os.Remove("/tmp/$$$mytoken")
+		os.Exit(0)
+	}
+	if len(myurl) == 0 || len(userid) == 0 {
 		usage()
 	}
 
+	// prompt for password
+	fmt.Printf("Enter your password:")
+	if pass, err = gopass.GetPasswd(); err == nil {
+		password = string(pass)
+	} else {
+		fmt.Println("error reading password", err)
+		os.Exit(1)
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 	// get the proxy environment variable
 	proxy := os.Getenv("HTTPS_PROXY")
 	if len(proxy) == 0 {
 		proxy = os.Getenv("https_proxy")
 	}
 
-	proxyUrl, _ := url.Parse(proxy)
-	if proxyUrl == nil {
-		fmt.Println("set HTTPS_PROXY or https_proxy environment variable")
-		os.Exit(1)
+	if len(proxy) != 0 {
+		proxyUrl, _ := url.Parse(proxy)
+		if proxyUrl != nil {
+			// update the transport to add Insecure connection and proxy
+			tr.Proxy = http.ProxyURL(proxyUrl)
+		}
 	}
-	// update the transport to add Insecure connection and proxy
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		Proxy:           http.ProxyURL(proxyUrl),
-	}
+
 	myurl = myurl + "/login"
+
 	client := &http.Client{Transport: tr}
 	req, _ := http.NewRequest("GET", myurl, nil)
 	req.SetBasicAuth(userid, password)
-	/*
-		form := url.Values{}
-		form.Add("u", userid)
-		form.Add("p", password)
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	*/
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 	if err != nil {
 		fmt.Println("login error:", err)
 	} else {
-		token, err = ioutil.ReadAll(resp.Body)
-		fmt.Println(string(token))
+		if token, err = ioutil.ReadAll(resp.Body); err == nil {
+			mytok := MyToken{}
+			if err = json.Unmarshal(token, &mytok); err == nil {
+				fmt.Println(string(mytok.Token))
+				if err = ioutil.WriteFile("/tmp/$$$mytoken", []byte(mytok.Token), 0700); err != nil {
+					fmt.Println("Error saving token", err)
+				}
+			} else {
+				fmt.Println("Invalid token data", err)
+			}
+		} else {
+			fmt.Println("Error reading token", err)
+		}
 	}
 
 }
--- app/client/fetch/fetch.go
+++ app/client/fetch/fetch.go
@@ -14,15 +14,17 @@ import (
 )
 
 var (
-	myurl, contentc        string
-	concurrent, loop, wait int
-	nbytes                 int64
-	mbytes                 []byte
-	content                bool
+	myurl, contentc, securec string
+	concurrent, loop, wait   int
+	nbytes                   int64
+	mbytes                   []byte
+	mytoken                  []byte
+	content, secure          bool
+	err                      error
 )
 
 func usage() {
-	usage := "\nUsage: fetch  -wwww  string -c  integer -l integer -W integer -content boolean" +
+	usage := "\nUsage: fetch -www <string> -S <boolean> -c <integer> -l <integer> -W <integer> -C <boolean>" +
 		"\n\nDefault Options:\n"
 	fmt.Println(usage)
 	flag.PrintDefaults()
@@ -33,34 +35,43 @@ func main() {
 
 	flag.Usage = usage
 	flag.StringVar(&myurl, "www", "", "web page to be fetched")
+	flag.StringVar(&securec, "S", "false", "secured web page")
 	flag.IntVar(&concurrent, "c", 1, "number of concurrent requests")
 	flag.IntVar(&loop, "l", 1, "number of times of concurrent requests")
 	flag.IntVar(&wait, "W", 50, "Wait time (ms) between 2 concurrent requests")
-	flag.StringVar(&contentc, "content", "false", "return the content if true otherwise the length of the web page")
+	flag.StringVar(&contentc, "C", "false", "return the content if true otherwise the length of the web page")
 
 	flag.Parse()
 	if len(myurl) == 0 {
 		usage()
 	}
 	content, _ = strconv.ParseBool(contentc)
-	fmt.Println(content)
+	secure, _ = strconv.ParseBool(securec)
 	waitTime := time.Duration(wait) * time.Millisecond
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		//Proxy:           http.ProxyURL(proxyUrl),
+	}
 
 	// get the proxy environment variable
 	proxy := os.Getenv("HTTPS_PROXY")
 	if len(proxy) == 0 {
 		proxy = os.Getenv("https_proxy")
 	}
-
-	proxyUrl, _ := url.Parse(proxy)
-	if proxyUrl == nil {
-		fmt.Println("set HTTPS_PROXY or https_proxy environment variable")
-		os.Exit(1)
+	if len(proxy) != 0 {
+		if proxyUrl, _ := url.Parse(proxy); proxyUrl != nil {
+			// update the transport to add Insecure connection and proxy
+			tr.Proxy = http.ProxyURL(proxyUrl)
+		}
 	}
-	// update the transport to add Insecure connection and proxy
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		Proxy:           http.ProxyURL(proxyUrl),
+
+	if secure {
+		if mytoken, err = ioutil.ReadFile(("/tmp/$$$mytoken")); err != nil {
+			fmt.Println("Please login first", err)
+			fmt.Println("Switch to unsecured request")
+		} else {
+			// fmt.Println(string(mytoken))
+		}
 	}
 
 	// start := time.Now()
@@ -68,42 +79,54 @@ func main() {
 	for k := 0; k < loop; k++ {
 		start := time.Now()
 		for i := 0; i < concurrent; i++ {
-			go fetch(myurl, tr, ch) // start a go routine
+			go fetch(myurl, mytoken, tr, ch) // start a go routine
 		}
-
 		for i := 0; i < concurrent; i++ {
 			fmt.Println(<-ch)
 		}
-
 		fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 		time.Sleep(waitTime)
 	}
-
 }
 
-func fetch(url string, tr *http.Transport, ch chan<- string) {
+func fetch(url string, mytoken []byte, tr *http.Transport, ch chan<- string) {
 
 	start := time.Now()
 	client := &http.Client{Transport: tr}
-	req, _ := http.NewRequest("GET", url, nil)
-	resp, err := client.Do(req)
-	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch return
+	if len(mytoken) > 0 {
+		url = url + "/secure"
 	}
+	req, _ := http.NewRequest("GET", url, nil)
+	if len(mytoken) > 0 {
+		req.Header.Add("Authorization", "Bearer "+string(mytoken))
+		// fmt.Println(req.Header)
+	}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		// request error then  immediat return
+		ch <- fmt.Sprint(err) // send to channel ch  and return
+		return
+	}
+
+	defer resp.Body.Close() // don't leak resources
+
+	// read the resp Body
 	if !content {
 		nbytes, err = io.Copy(ioutil.Discard, resp.Body)
 	} else {
 		mbytes, err = ioutil.ReadAll(resp.Body)
 	}
-	resp.Body.Close() // don't leak resources
+
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+
 	secs := time.Since(start).Seconds()
 	if !content {
 		ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 	} else {
-		ch <- fmt.Sprintf("%.2fs %7d %s", secs, string(mbytes), url)
+		ch <- fmt.Sprintf("%.2fs %s %s", secs, string(mbytes), url)
 	}
 }
